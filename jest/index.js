2eb2fdc84ad30340fcc49f4797e8288a02291b54
edonet <edonet@163.com>
2018-03-10 15:50:42 +0800

    init

---
+++ jest/index.js
@@ -0,0 +1,47 @@
+/**
+ *****************************************
+ * Created by lifx
+ * Created on 2017-11-27 14:15:35
+ *****************************************
+ */
+'use strict';
+
+
+/**
+ *****************************************
+ * 加载依赖
+ *****************************************
+ */
+const
+    path = require('path'),
+    dir = (...args) => path.resolve(__dirname, ...args),
+    resolve = require.resolve;
+
+
+/**
+ *****************************************
+ * 输出配置
+ *****************************************
+ */
+module.exports = {
+    rootDir: dir('../'),
+    roots: [
+        '<rootDir>/lib/'
+    ],
+    testMatch: [
+        '**/?(*.)(spec|test).js'
+    ],
+    testEnvironment: 'jsdom',
+    testURL: 'http://localhost',
+    transform: {
+        '^.+\\.(js|jsx)$': resolve('./jsTransform.js'),
+        '^.+\\.(scss|css)$': resolve('./cssTransform.js'),
+        '^(?!.*\\.(js|jsx|css|json)$)': resolve('./fileTransform.js')
+    },
+    transformIgnorePatterns: [
+        'node_modules', 'git'
+    ],
+    collectCoverageFrom: [
+        '**/*.{js,jsx}'
+    ]
+};
